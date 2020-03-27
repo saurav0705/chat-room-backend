58881ec4bde7857026ad7ea7d53c652a1602cfff
saurav0705 <sauravaggarwal98@gmail.com>
2020-03-27 13:37:32 +0530

    cors moved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
-
+app.use(cors());
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(cors());
+
 app.use(router);
 
 io.on('connect', (socket) => {
