f9fa69e9ecb4eb7261f9c169472929f38fa7ad77
saurav0705 <sauravaggarwal98@gmail.com>
2020-03-30 17:20:07 +0530

    mock data added

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mock = require('./mock.json');
 const {getAll,getRooms} = require('./users');
 router.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
@@ -14,6 +15,13 @@ router.get("/rooms",(req,res)=>{
   res.statusCode = 200;
   res.setHeader('Content-type','application/json');
   res.json(getRooms());
+})
+
+
+router.get('/mock-data',(req,res)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-type','application/json');
+  res.json(mock);
 })
 
 module.exports = router;
