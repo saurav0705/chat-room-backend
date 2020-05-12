af0422e385fcef6b717ed970f6d29df690ddef83
saurav0705 <sauravaggarwal98@gmail.com>
2020-05-12 17:47:34 +0530

    mock server data added

--- router.js
+++ router.js
@@ -22,6 +22,28 @@ router.get('/mock-data',(req,res)=>{
   res.statusCode = 200;
   res.setHeader('Content-type','application/json');
   res.json(mock);
+});
+
+router.get('/mock-json',(req,res)=>{
+  let obj = {
+    "odd":[],
+    "even":[],
+    "fibbonaci":[],
+    "factorial":[]
+}
+
+  let promises = Object.keys(obj).map( key => {
+  for(let i=0;i<parseInt(Math.random()*100);i++){
+    obj[key].push(parseInt(Math.random()*100));
+  }
+  return obj;
+  
+})
+Promise.all(promises).then(function () {
+  res.statusCode=200;
+  res.setHeader('Content-type','application/json');
+  res.json(obj)
+});
 })
 
 module.exports = router;
